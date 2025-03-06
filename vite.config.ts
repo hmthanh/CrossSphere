b5192f0214d152b0cb552b5dd76ebd212f6b5bfe
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-03-06 19:13:27 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CrossSphere/', // Replace with your repository name
+  base: '/CrossSphere/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
